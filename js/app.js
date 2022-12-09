8c98e5d10362005c2fb8bbb322c6b73ea410a59d
k-vernon <kvernon908@gmail.com>
2022-12-08 19:02:34 -0500

    set variables and complete updateBoard function

--- js/app.js
+++ js/app.js
@@ -3,16 +3,49 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let board = [null, null, null, null, null, null, null, null, null]
+console.log(board);
+let turn = 1
+console.log(turn);
+let winner = false
+console.log(winner);
+let tie = false
+console.log(tie);
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const squareEls = document.querySelectorAll(".sqr")
+console.log(squareEls)
+const messageEl = document.getElementById("message")
+console.log(messageEl);
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+function init(){
+    render()
+    console.log(init)
+}
+init()
 
+function render(){
+
+}
+
+function updateBoard(){
+    board.forEach(function(square, idx){
+        if (square === 1) {
+            squareEls[idx].textContent = "X"
+        } else if (square === -1){
+            squareEls[idx].textContent = "O"
+        } else {
+            squareEls[idx].textContent = ""
+        }
+        
+        console.log(square)
+    })
+}
+updateBoard()
+
+//
